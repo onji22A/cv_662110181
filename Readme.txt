dabece31af5f9b18e8bda9abff3f56c77234fa58
onji22A <113840059+onji22A@users.noreply.github.com>
2025-08-11 06:25:12 +0000

    cv

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+onjira thiraphot
+662110181
+
+https://github.com/onji22A/cv_662110181.git
+
---
+++ script.js
@@ -0,0 +1,6 @@
+function viewContact() {
+
+    alert("Email: onjira@cmu.com\nPhone: +66 123-456-789");
+    
+    
+}
